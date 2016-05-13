4bcadf549070521f4efd141f7f9bea1b41946e17
Ibrahim Alyahya <ibr.alyahya@gmail.com>
2016-05-13 11:45:08 -0400

    [ADD] Logging to task 3

--- Lab1/Task3/3_1.c
+++ Lab1/Task3/3_1.c
@@ -1,76 +1,91 @@
 #define _BSD_SOURCE
-#include<stdio.h>
-#include<dirent.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
 
-int MAX_FORKS;
+int MAX_FORKS; 
 int fr=0; //failratio in percent
 
-int backup_terminated(int* fd){
-	char c[] = "test";
-	int ret = write(fd[1], c, strlen(c)+1);
-	//printf("write feedback %i \n",ret);
+void logThis(char* text, int arg){
+	// Get the log file or create a new one	
+	FILE *logFile = fopen("log.txt", "a");
+	
+	// Check of the file could be opened
+	if (logFile == NULL){
+		perror("fopen failed to open log.txt file!\n");
+		return;
+	}
+
+	// The string to be printed 
+	char logText[100]; 
+	sprintf(logText, text, arg);
+	
+	// Print the string in the log file
+	fprintf(logFile, logText);
+	
+	// Close the log file
+	fclose(logFile);
+}
+
+void backup_terminated(int* fd){
+	// Prepare the arguments in case the parent was terminated
 	char arg1[10], arg2[10];
 	sprintf (arg1 , "%s%d" , "-n " , MAX_FORKS);
 	sprintf (arg2 , "%s%d" , "-f " , fr);
+	
+	// Try to write in the pipe
+	char c[] = "test";
+	int ret = write(fd[1], c, strlen(c)+1);
+
+	// Check if it was not possible to write there
 	if(ret == -1){
-		 if(errno == EPIPE){
-		    execl("run", "run", arg1, arg2, (char*)0 );
-		    
-		   // printf("EPIPE\n");
-		 }
-		if(errno == EAGAIN)
-	    	printf("EAGAIN\n");
-		  if(errno == EBADF)
-		    printf("EBADF\n");
-		  if(errno == EDESTADDRREQ)
-		    printf("EDESTADDRREQ\n");
-		  if(errno == EDQUOT)
-		    printf("EDQUOT\n");
-		  if(errno == EFAULT)
-		    printf("EFAULT\n");
-		  if(errno == EFBIG)
-		    printf("EFBIG\n");
-		  if(errno == EINTR)
-		    printf("EINTR\n");
-		  if(errno == EINVAL)
-		    printf("EINVAL\n");
-		  if(errno == EIO)
-		    printf("EIO\n");
-		  if(errno == ENOSPC)
-		    printf("ENOSPC\n");	  
+		// Check if the read end was closed -> parent was terminated
+		if(errno == EPIPE){
+			logThis("Child detects the termination of its parent and will take the role as a backup process!\n", 0);
+			logThis("---\n", 0);
+			logThis("The state of its parent was:\n", 0);
+			logThis("Max Forks = %d\n", MAX_FORKS);
+			logThis("Fail Ratio = %d\n", fr);
+			logThis("---\n", 0);
+			logThis("-----------------------------RESTARTING-----------------------------\n", 0);
+			execl("run", "run", arg1, arg2, (char*)0);
+      		perror("execl() failure!\n");
+		} else
+			perror("Could not write even that the parent was not terminated!");
 	}
 }
 
 int server(int fr, int* fd){
-	//reserve memory for reading request files
+	// Reserve memory for reading request files
 	char buffer[255];
 	struct dirent* entry;
 	while(1){
-		
-
-		//open directory stream
+		// Open a directory stream of requests
 		DIR *requestdir	= opendir("./requests");
-		while( (entry = readdir(requestdir)) != NULL){
-
-		//call backup terminated before opening a new request
+		
+		// Loop on each file in this directory
+		while((entry = readdir(requestdir)) != NULL){
+			// Check if the parent process was terminated, otherweise continue
 			backup_terminated(fd);
+			
+			// Check if this file has only a regular type
 			if (entry->d_type != DT_REG){
 				continue;
 			}
+			
+			// Open the file
 			char filepath[100];
 			strcpy(filepath, "requests/");
 			strncat(filepath, entry->d_name, 22);
-			//only parse regular files
 			FILE* requestFile = fopen(filepath, "r");
 			
-			//create artificial crash if specified by -f and filename contains "fail"
+			// Create artificial crash if specified by -f and filename contains "fail"
 			const char failstr[10] = "fail";
 			char* ret;
 			ret = strstr(entry->d_name,failstr);
@@ -82,10 +97,13 @@ int server(int fr, int* fd){
 				}
 			}
 			
-			//only read in first line (max 255 chars), since this is only pseudo code
+			// Only read in first line (max 255 chars), since this is only pseudo code
 			char* request = fgets(buffer, 255, requestFile);
+
 			printf("server [%d] req: %s\n", getpid(), entry->d_name);
 			usleep(500000);
+
+			// Close the file and delete it
 			fclose(requestFile);
 			unlink(filepath);
 		}
@@ -97,55 +115,77 @@ int backup(int MAX_FORKS, int* fd) {
 	int pid;
 	int status;
 	int sleep_status;
-	int num_forks= 0;
-	//create primary processes and wait in case they fail
+	int printParentID = 1; // will be used to check if parent ID was logged
+	int num_forks = 0;
+
+	// Create primary processes and wait in case they fail
 	while(1){
-		//count number of attempts at child creation
+		// Count number of attempts at child creation
 		num_forks+=1;
-		printf("creating child...\n");
+		
+		// Create a pipe to be used as a channel between parent and its child
 		pipe(fd);
-		//ignore the Signal
+
+		// Ignore the signal
 		signal(SIGPIPE, SIG_IGN);
+
+		printf("creating child...\n");
 		pid = fork();
-		//handle fork error
+
+		// Handle fork error
 		if(pid < 0) {
 			perror("error when creating a child");
 		}
-		//child process(primary)
+
+		// Child process (primary)
 		if(pid == 0) {
+			// The child closes the write end of the pipe
 			close(fd[0]);
-			printf("i am the child process with pid %i\n",getpid());
+
+			printf("I am the child process with pid %i\n", getpid());
+			logThis("A new child process created with ID = %i\n", getpid());
+
 			//return to start request processing
 			return 0;
 		}
 		else if(pid < 0 && num_forks > MAX_FORKS){
-			//gracefully degrade and start request processing without backup
-			printf("i am the parent process with pid %i\n",getpid());
+			// Gracefully degrade and start request processing without backup
+			printf("I am the parent process with pid %i\n",getpid());
 			perror("Maximum number of retries to create child exceeded, backup process is now doing processing");
 			return pid;
 		}
 
-			printf("i am the parent process with pid %i\n",getpid());
+		printf("I am the parent process with pid %i\n",getpid());
+		
+		// Check if the parent ID was not printed in the log file
+		if(printParentID){
+			logThis("The parent process has ID = %i\n", getpid());
+			printParentID = 0;
+		}
+		
+		// The parent closes the read end of the pipe
 		close(fd[1]);
-		//wait for crash of primary
+
+		// Wait for crash of primary
 		printf("waiting %i\n", pid);
-		pid = waitpid((pid_t)pid,&status,0);
-		printf("done waiting%i\n", pid);
-		//handle waitpid error
+		pid = waitpid((pid_t)pid, &status, 0);
 		if(pid < 0){
 			perror("Error while trying to wait for child");
 		}
+
+		printf("done waiting%i\n", pid);
 	}
 	return 0;
 }
 
-
 int main(int argc, char** argv){
 	printf("A nice welcome message\n");
-	int c; 
-	int fd[2];
+	logThis("The program starts with a very nice welcome!\n", 0);
+
+	int c; // Reserved for reading the program arguments
+	int fd[2]; // Reserved for a pipe, the communication channel between parent and child
 	
-	//parse input
+	// Parse inputs
 	while ((c = getopt(argc, argv, "n:f:")) != -1){
 		switch(c){
 			case 'n':
@@ -166,15 +206,23 @@ int main(int argc, char** argv){
 				fprintf(stderr, "Unknown or syntactically erroneous parameter\n");
 		}
 	}
-//	printf("##############################%i, %i############################",MAX_FORKS, fr);
-	//start creating processes
+
+	// Logging program arguments
+	logThis("---\n", 0);
+	logThis("The program has the following arguments:\n", 0);
+	logThis("Max Forks = %d\n", MAX_FORKS);
+	logThis("Fail Ratio = %d\n", fr);
+	logThis("---\n", 0);
+	
+	// Start creating processes
 	backup(MAX_FORKS, fd);
-	//init rng after backup has been done to get different seeds
+	
+	// Init rng after backup has been done to get different seeds
 	srand(getpid());
-	printf("start processing\n");
-	//start processing requests
+	
+	printf("start processing requests\n");
 	int ret = server(fr, fd);
+	
 	printf("done processing\n");
 	exit(ret);
-
 }
