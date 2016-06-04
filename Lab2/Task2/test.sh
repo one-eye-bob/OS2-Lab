6a1cccd8d998ce7e472f930b1112b95b01f1cb8f
Amos <amos.treiber@gmail.com>
2016-06-04 18:27:46 +0200

    Enhanced solution for 2.4 still not working

--- Lab2/Task2/test.sh
+++ Lab2/Task2/test.sh
@@ -4,38 +4,51 @@
 #	opfn: the filename of output (the reguired first argument of EXEC)
 #	cpi: the checkpoints interval in seconds
 
-echo TEST# Starting
+#echo TEST# Starting
 
 #RUN the program
-#sudo ./$EXEC $opfn $cpi 
+#sudo ./$EXEC $opfn $cpis
 
 #get the process ID of the worker; to compare it later with the ID of the restored process after crash
-workerpid=$! # (not working) This should get the pid of the worker
+#workerpid=$! # (not working) This should get the pid of the worker
+rounds=10
+fr=35
+counter=0
+clen=0
+while [ $counter -lt $rounds ]
+do
+	sleep 1
+	f=$((RANDOM % 100))
+	n=$((RANDOM % 10 + 1))
+	if [ $f -le $fr ]; then
+		n=$((0 - $n))
+	fi
+	sleep 2
+	echo $n
+	((counter++))
+	clen=$(($clen + $n))
+done
 
+#enter a non-number char(s); to end the program and write all generated number to the output file
+echo x
 
-#enter random "small" positve integers n1, n2, n3 (TODO) to stdin, one after the other
-n1=$((RANDOM % 10))
-n2=$((RANDOM % 10))
-n3=$((RANDOM % 10))
+sleep 1
 
-#two possibilities:
-#	kill the worker process by external signal (such as SIGKILL)
-#	kill $workerpid
-#OR
-#	enter a random negative number (TODO) to stdin; to make the woker crashes
-	n4=$((-RANDOM))
-#	check if this woker process exited with a value equals to this negative number
-#	$?==$n4
 
 #TEST-1: check if a process with the ID the previus worker is running
 #$workerpid==$!
 
 #TEST-2:
-#enter a non-number char(s); to end the program and write all generated number to the output file
-#check if the data of the generated numbers is not lost by:
-#	(1) comparing the number of lines equals n1+n2+n3+1 (the last return char)
-#	(2) each number must be not bigger than their range, whether n1, n2 or n3
+#see if we have the desired output (legth check only)
+content='cat $opfn'
+alen=${#content}
+if (($clen == $alen)); then
+	echo "Test successful"
+else
+	echo "$opfn contains different numbers than should have been generated!"
+fi
 
-echo TEST# The end
+#kill processes
+sudo pkill -f "./$EXEC $opfn $cpi"
 
-
+#echo TEST# The end
